38cd8654b7217b276115ec3584002fb244724d6d
Risav Arrahman <risavbm@gmail.com>
2022-07-21 19:11:17 +0700

    upload to git

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      fontFamily: {
+        inter: ['Inter'],
+      },
+      colors: {
+        primary: '#42868E',
+        lightblue: '#A6D9DB',
+        red: '#dc2626',
+      },
+      screens: {
+        '2xl': '1520px',
+      },
+    },
+  },
+  plugins: [],
+};
